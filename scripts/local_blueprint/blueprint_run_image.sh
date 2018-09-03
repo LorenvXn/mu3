7c24876782452cc9570c1d85a11ffe705f75d21b
Loren vXn <predark.dante@gmail.com>
2018-09-03 18:12:46 +0300

    Update blueprint_run_image.sh

--- scripts/local_blueprint/blueprint_run_image.sh
+++ scripts/local_blueprint/blueprint_run_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker build -t containerimage:tagimage /opt/remote_shift/kafka #lol! such amateur!
+                                                                #beware, the path must be changed 
+                                                                # whatever the image
 
