b1be128213d605bc01e4bb594185b7754388f708
Loren vXn <predark.dante@gmail.com>
2018-09-04 02:17:09 +0300

    Create blueprint_run_image.sh

---
+++ scripts/local_blueprint/kafka/blueprint_run_image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t containerimage:tagimage /opt/remote_shift/kafka 
+                                                               
+                                                               
