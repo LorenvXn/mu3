6a9ccbe70801acfeea0481fefc2168fcbcaa3edf
Loren vXn <predark.dante@gmail.com>
2018-09-10 19:38:28 +0300

    Create test.sh

---
+++ network/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONTAINER=$1
+KAFKA_IP=$2
+ZOO_IP=$3
+
+#docker exec $CONTAINER ls
+#lol! such 1337 skillz
+
+docker exec $CONTAINER \
+sed -i 's/#advertised/advertised/g;s/your.host.name/'"$KAFKA_IP"'/g;s/localhost/'"$ZOO_IP"'/g' /opt/kafka/config/server.properties 
+
