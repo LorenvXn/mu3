f14181ab0d0611dace922b2a826e3b38b0cc28f3
Loren vXn <predark.dante@gmail.com>
2018-09-10 17:39:56 +0300

    Update blueprint_param_run_container.sh

--- scripts/local_blueprint/blueprint_param_run_container.sh
+++ scripts/local_blueprint/blueprint_param_run_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker create -i -t --name containername -p porthost:portcontainer \
+docker create -i -t  --name namecontainer -p porthost:portcontainer   \
+--storage-opt size=amountgb \
 --cap-add=ALL  `docker images | grep tagimage | awk {'print $3'} | grep -vi image` /bin/bash
 
 sleep 5
 
-docker start containername
+docker start namecontainer
