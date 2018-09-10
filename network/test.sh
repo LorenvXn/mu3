7a2e7d2f1e0f5ab771257b9cc3e2af1167397ba9
Loren vXn <predark.dante@gmail.com>
2018-09-10 19:54:02 +0300

    Update test.sh

--- network/test.sh
+++ network/test.sh
@@ -10,9 +10,10 @@ ZOO_IP=$3
 
 #testing - testing -----------------------
 #docker exec $CONTAINER ls
-#lol! such 1337 skillz ... thanks, port forwarding!
+
 
 docker exec $CONTAINER \
 sed -i 's/#advertised/advertised/g;s/your.host.name/'"$KAFKA_IP"'/g;s/localhost/'"$ZOO_IP"'/g' \
 /opt/kafka/config/server.properties 
 
+#lol! such 1337 skillz ... thanks, port forwarding!
